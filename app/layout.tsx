0bb10d1d29f4d5fb4d0dc711c681173dfba4eee5
void-main1812 <sour.bt21cs56@opju.ac.in>
2023-06-15 23:23:49 +0530

    :lipstick: Add: SideBar Setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,27 @@
 import './globals.css'
-import { Figtree } from 'next/font/google'
+import {Figtree} from 'next/font/google'
 import React from "react";
+import Sidebar from "@/app/components/Sidebar";
 
-const font = Figtree({ subsets: ['latin'] })
+const font = Figtree({subsets: ['latin']})
 
 export const metadata = {
-  title: 'Groove-box',
-  description: 'A spot where whole world can grooves',
+    title: 'Groove-box',
+    description: 'A spot where whole world can grooves',
 }
 
 export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
+                                       children,
+                                   }: {
+    children: React.ReactNode
 }) {
-  return (
-    <html lang="en">
-      <body className={font.className}>{children}</body>
-    </html>
-  )
+    return (
+        <html lang="en">
+        <body className={font.className}>
+        <Sidebar>
+            {children}
+        </Sidebar>
+        </body>
+        </html>
+    )
 }
