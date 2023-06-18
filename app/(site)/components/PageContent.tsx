1cb6fe4b99e8c9b911c11e252b962b914362da83
void-main1812 <sour.bt21cs56@opju.ac.in>
2023-06-19 01:37:28 +0530

    🔄🎵 Updated: Fetching songs in home page and library

--- app/(site)/components/PageContent.tsx
+++ app/(site)/components/PageContent.tsx
@@ -1,11 +1,45 @@
 'use client'
 
 import React from 'react';
+import {Song} from "@/types";
+import {AiOutlineExclamation} from "react-icons/ai";
+import SongItem from "@/app/components/SongItem";
 
-const PageContent = () => {
+interface PageContentProps {
+    songs: Song[];
+}
+
+const PageContent: React.FC<PageContentProps> = ({songs}) => {
+    if (songs.length === 0) {
+        return (
+            <div className='mt-4 text-neutral-600 justify-center items-center flex text-2xl font-bold '>
+                <AiOutlineExclamation size={26}/>
+                No songs Available
+            </div>
+        )
+    }
     return (
-        <div>
-            Page Content
+        <div
+            className='
+            grid
+            grid-cols-1
+            sm:grid-cols-3
+            md:grid-cols-3
+            lg:grid-cols-4
+            xl:grid-cols-5
+            2xl:grid-cols-8
+            gap-4
+            mt-4
+            '
+        >
+            {songs.map((item) => (
+                <SongItem
+                    key={item.id}
+                    onClick={() => {
+                    }}
+                    data={item}
+                />
+            ))}
         </div>
     );
 };
