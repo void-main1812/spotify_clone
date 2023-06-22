bdfd849603f466a08f70e71ca2cf99c5027a70f4
void-main1812 <sour.bt21cs56@opju.ac.in>
2023-06-22 23:04:32 +0530

    Minor UI change for Mobile Screen

--- app/(site)/components/PageContent.tsx
+++ app/(site)/components/PageContent.tsx
@@ -24,7 +24,7 @@ const PageContent: React.FC<PageContentProps> = ({songs}) => {
         <div
             className='
             grid
-            grid-cols-1
+            grid-cols-2
             sm:grid-cols-3
             md:grid-cols-3
             lg:grid-cols-4
